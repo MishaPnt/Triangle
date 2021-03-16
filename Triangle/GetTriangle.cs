245c09d63fdbb02b5c10ca00d9eebae70f8e6825
Misha <konokradkonokrad@gmail.com>
2021-03-16 13:26:02 +0200

    commit

---
+++ Triangle/GetTriangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+
+    class GetTriangle : Triangle
+    {
+
+        public double heigth;
+        public double baseOfTriangle;
+        public int a;
+        public int b;
+        public int c;
+        public GetTriangle(double heigth, double baseOfTriangle, int a, int b, int c)
+        {
+            this.heigth = heigth;
+            this.baseOfTriangle = baseOfTriangle;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+        override public double GetArea()
+        {
+            return 1.0 / 2 * baseOfTriangle * heigth;
+        }
+        override public int GetPerimetr()
+        {
+            return a + b + c;
+        }
+    }
+    abstract class Triangle
+    {
+        abstract public double GetArea();
+        abstract public int GetPerimetr();
+    }
+}
--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Triangle;
 
-namespace Triangle
+namespace TTriangle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Висота: ");
+            double heigth = double.Parse(Console.ReadLine());
+            Console.WriteLine("Основа Трикутника: ");
+            double baseOfTriangle = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона а: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона c: ");
+            int c = int.Parse(Console.ReadLine());
+            GetTriangle getTriangle = new GetTriangle(heigth, baseOfTriangle, a, b, c);
+            Console.WriteLine("Площа трикутника: ");
+            Console.WriteLine(getTriangle.GetArea());
+            Console.WriteLine("Периметр трикутника: ");
+            Console.WriteLine(getTriangle.GetPerimetr());
+
         }
     }
 }
