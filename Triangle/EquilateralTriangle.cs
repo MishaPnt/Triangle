c5079367f565f990a2ff7ab728d8d38b04ad631e
Misha <konokradkonokrad@gmail.com>
2021-03-18 22:27:22 +0200

    add new class

---
+++ Triangle/EquilateralTriangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Triangles;
+
+namespace Triangle
+{
+    class EquilateralTriangle : GetTriangles
+    {
+        public EquilateralTriangle(int a, int b, int angle) : base(a, b, angle)
+        {
+
+        }
+
+        public override double Area => throw new NotImplementedException();
+
+        public override int Perimetr => throw new NotImplementedException();
+
+        public override double AngleA => throw new NotImplementedException();
+
+        public override double AngleB => throw new NotImplementedException();
+
+        public override double AreaOfRectangularTriangle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double Hypotenuse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double TriangleIsoscel()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
